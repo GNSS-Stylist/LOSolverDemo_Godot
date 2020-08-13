21d09dff209a6d25a1c98eab9f0ace8d38f59df6
Pasi Nuutinmaki <GNSSStylist@sci.fi>
2020-08-13 22:18:57 +0300

    Add some comments

--- CarWheel.gd
+++ CarWheel.gd
@@ -16,6 +16,14 @@ func _process(delta):
 	var parent = self.get_parent()
 	var globalMovement = parent.global_transform.origin - lastGlobalOrigin
 
-	var forwardMovement = parent.global_transform.basis.z.dot(globalMovement) / parent.global_transform.basis.z.length()
+	# "Forward" as "seen" by the wheel
+	var forwardMovement = parent.global_transform.basis.z.dot(globalMovement)
+	
+	# This is to make the wheel turning speed to slow down when the car is scaled up
+	# (Double size car -> halve the speed)
+	forwardMovement /= parent.global_transform.basis.z.length()
+	
+	# This will accumulate rounding error over time, but since the rotation
+	# axis is basis.x, it will not turn the wheel "out of it's place"
 	self.transform = self.transform.rotated(self.transform.basis.x, forwardMovement * rotationSpeed * delta).orthonormalized()
 	lastGlobalOrigin = parent.global_transform.origin
